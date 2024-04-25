3e5abd4f23be972f1b31f97834d7ce6bc94ea98c
thosaphol <thosaphol@outlook.co.th>
2024-04-25 22:47:13 +0700

    [MOD] Set Default value in table deductions

--- init.sql
+++ init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS deductions (
-    personal float NOT NULL,
-    maximum_k_receipt float NOT NULL
+    personal float NOT NULL DEFAULT 0,
+    maximum_k_receipt float NOT NULL DEFAULT 0
 );
 
 INSERT INTO deductions VALUES
